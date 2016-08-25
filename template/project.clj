5c057f99a5baa10b7227753ab3ac9f2feddc2818
hamidr <hamidr.dev@gmail.com>
2016-08-25 17:48:12 +0430

    yup

--- template/project.clj
+++ template/project.clj
@@ -1,5 +1,10 @@
-(defproject clojureapp "0.1.0-SNAPSHOT"
-    :plugins [[lein-ring "0.7.5"]]
-    :dependencies [[org.clojure/clojure "1.5.1"]
-                   [http-kit "2.1.12"]]
-    :main webapp)
+(defproject cata "0.1.0"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [funcool/catacumba "1.0.0"]]
+  :main ^:skip-aot cata.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
